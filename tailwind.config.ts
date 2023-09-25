3d631c0fc7d561c51a3fdd98c4d861520e3f4c7b
Lakee Sivaraya <lakeesivaraya@gmail.com>
2023-09-25 13:22:25 +0100

    styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./src/popup/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bcm-orange": "#EE3C35",
+        "bcm-orange-light": "#ff5a38",
+      },
+      fontFamily: {
+        iceland: ["Iceland", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
